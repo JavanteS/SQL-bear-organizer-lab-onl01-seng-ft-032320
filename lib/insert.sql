c1af7d1b088279f81cf500b90559fd5809f01e7e
Javante Nelson <javantenelson@gmail.com>
2020-04-19 23:57:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,3 +8,5 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ("Rowdy", 4, "M", "black","wild", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ("Rowdy", 4, "M", "black","wild", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Rowdy", 4, "M", "black","wild", 1);
