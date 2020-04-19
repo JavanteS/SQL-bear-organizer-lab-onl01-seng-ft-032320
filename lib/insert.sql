53c797c1843b2f645f73f8020d8a3f1c93453494
Javante Nelson <javantenelson@gmail.com>
2020-04-19 23:53:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ("Mr.Chocolate", 11, 3.4, 3);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr.Chocolate", 11, 3.4, 3);
