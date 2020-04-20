8fcbc04901f87826fda9af28dd57e366bea25817
Javante Nelson <javantenelson@gmail.com>
2020-04-20 00:42:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,8 @@ def selects_oldest_bear_and_returns_name_and_age
   "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
-def 
+def select_youngest_bear_and_returns_name_and_age
+  
 end
 
 def selects_most_prominent_color_and_returns_with_count
