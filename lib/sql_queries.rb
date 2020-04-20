df6dcc36700f7532f0610225b1790e800e04223d
Javante Nelson <javantenelson@gmail.com>
2020-04-20 00:41:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,7 @@ def selects_oldest_bear_and_returns_name_and_age
   "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end
 
-defSELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
+def
 end
 
 def selects_most_prominent_color_and_returns_with_count
